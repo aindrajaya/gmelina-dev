d3625b2e245d39d77968a9ade772c79bba681f29
Arista Indrajaya <arista.indrajaya@gmail.com>
2022-01-18 19:55:57 +0700

    for develop purpose only

---
+++ src/pages/api/products.js
@@ -0,0 +1,15 @@
+import { connectToDatabase } from "../../util/mongodb";
+import data from '../../../products.json'
+
+export default async (req, res) => {
+  console.log(data)
+  try {
+    const { db } = await connectToDatabase();
+    let products = await db.collection("products").find({}).toArray();
+    products = JSON.parse(JSON.stringify(products));
+    return res.status(200).json(products);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  mode: "jit",
+  purge: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: false,
+  theme: {
+    fontFamily: {
+      Poppins: ["Poppins", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        blue: {
+          light: "#b0d6af",
+          dark: "#9dc09f"
+        },
+        blueDefault: {
+          light: "#0a81ab",
+          dark: "#0c4271",
+        }
+      },
+    },
+    screens: {
+      xxs: "375px",
+      xs: "425px",
+      sm: "640px",
+      md: "780px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/line-clamp")],
+};
